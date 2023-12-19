2d77649f46bb3026eee3db85c65fa03d21b47056
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 14:45:38 +0530

    done

--- my-book-library/src/Compoents/BorrowHistory.js
+++ my-book-library/src/Compoents/BorrowHistory.js
@@ -6,10 +6,11 @@ const BorrowHistory = ({ history }) => {
       <h2>Borrow History</h2>
       <ul>
         {history.map((entry, index) => (
+            
           <li key={index}>
             <strong>{entry.title}</strong> - Borrowed by {entry.borrower} on {entry.date}
           </li>
-          
+
         ))}
       </ul>
     </div>
